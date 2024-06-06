9625a17b0ee020f4795cd80232966d78e22823e4
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-06 17:08:44 +0200

    Modify Header.css and main page test text

--- frontend/src/Pages/MainPage.jsx
+++ frontend/src/Pages/MainPage.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+
 const MainPage = () => {
 
     return (
-        <div>
-            Welcome to the Videogame Database!
+        <div className="MainPage">
+            <h1>Welcome to VGDB</h1>
+            <p>This is a video game database. Here you can find information about your favorite games.</p>
         </div>
     )
 }
