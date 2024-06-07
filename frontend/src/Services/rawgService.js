f4b748bbecf04ecd681b2418d3af7b136272fb1a
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-07 15:59:05 +0200

    Re-add api key to rawgService.js

--- frontend/src/Services/rawgService.js
+++ frontend/src/Services/rawgService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = process.env.RAWG_DB_API_KEY;
+const API_KEY = '63c6aaa2e6ac4ba3a87cb42f3d421cd9';
 const BASE_URL = 'https://api.rawg.io/api';
 
 export const fetchGames = async (page = 1, pageSize = 10) => {
