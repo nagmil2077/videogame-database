91f07c861a98d5b75dfd07ecc6df5500c7988126
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-11 12:08:20 +0200

    Create useEffect and fetchDetails function in GamePage.jsx

--- frontend/src/Pages/GamePage.jsx
+++ frontend/src/Pages/GamePage.jsx
@@ -1,9 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import { fetchGameDetails } from '../Services/rawgService';
 import './GamePage.css'
 
-const GamePage = () => {
-
+const fetchDetails = async (gameName) => {
+    try {
+        return await fetchGameDetails(gameName);
+    } catch (error) {
+        console.error("Error fetching game details:", error);
+    }
 }
+
+const GamePage = () => {
+    const { gameName } = useParams();
+    const [game, setGame] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetchDetails(gameName)
+            .then(data => setGame(data))
+    }, [gameName]);
+
+
+};
 
 export default GamePage;
