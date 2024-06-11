a19d1e2f55cd5513161e49557d062eab0a71c2de
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-11 12:07:56 +0200

    Create fetchGameDetails function in rawgService

--- frontend/src/Services/rawgService.js
+++ frontend/src/Services/rawgService.js
@@ -38,3 +38,17 @@ export const searchGames = async (query) => {
         throw error;
     }
 };
+
+export const fetchGameDetails = async (gameSlug) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/games/${gameSlug}`, {
+            params: {
+                key: API_KEY,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching game details:", error);
+        throw error;
+    }
+};
