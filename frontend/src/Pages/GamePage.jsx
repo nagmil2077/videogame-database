c391d643ecc02503e5e63e1cc09e21bbf1f63a13
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-11 11:17:36 +0200

    Create GamePage.jsx

---
+++ frontend/src/Pages/GamePage.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import './GamePage.css'
+
+const GamePage = () => {
+
+}
+
+export default GamePage;
