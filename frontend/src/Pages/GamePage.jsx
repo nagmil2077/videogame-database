a080687fe1696693b4419207deb158f6bdb3e1d3
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-11 12:14:52 +0200

    Create return for GamePage.jsx

--- frontend/src/Pages/GamePage.jsx
+++ frontend/src/Pages/GamePage.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import { fetchGameDetails } from '../Services/rawgService';
+import { Container, Row, Col, Button, Image } from 'react-bootstrap';
 import './GamePage.css'
 
 const fetchDetails = async (gameName) => {
@@ -21,7 +22,38 @@ const GamePage = () => {
             .then(data => setGame(data))
     }, [gameName]);
 
+    if (!game) {
+        return <div>Loading...</div>;
+    }
 
+    return (
+        <Container className="game-page">
+            <Button variant="secondary" onClick={() => navigate('/')}>Back</Button>
+            <Row className="mt-4">
+                <Col md={4}>
+                    <Image src={game.background_image} alt={game.name} fluid />
+                </Col>
+                <Col md={8}>
+                    <h1>{game.name}</h1>
+                    <p><strong>Release Date:</strong> {game.released}</p>
+                    <p><strong>Genre:</strong> {game.genres.map(genre => genre.name).join(', ')}</p>
+                    <p><strong>Description:</strong> {game.description_raw}</p>
+                </Col>
+            </Row>
+            <Row className="mt-4">
+                <Col>
+                    <iframe
+                        width="100%"
+                        height="500"
+                        src={`https://www.youtube.com/embed/${game.clip?.video}`}
+                        title="Game Trailer"
+                        frameBorder="0"
+                        allowFullScreen
+                    ></iframe>
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default GamePage;
