5651281f69bdae4475fecd0037f2c56f41eef121
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-13 13:06:38 +0200

    Modify rawgService.js functions to make requests to backend

--- frontend/src/Services/rawgService.js
+++ frontend/src/Services/rawgService.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
-const API_KEY = '63c6aaa2e6ac4ba3a87cb42f3d421cd9';
-const BASE_URL = 'https://api.rawg.io/api';
-
 export const fetchGames = async (page = 1, pageSize = 10) => {
     try {
-        const response = await axios.get(`${BASE_URL}/games`, {
+        const response = await axios.get(`/api/games`, {
             params: {
-                key: API_KEY,
                 page,
                 page_size: pageSize,
             },
@@ -20,16 +16,10 @@ export const fetchGames = async (page = 1, pageSize = 10) => {
 };
 
 export const searchGames = async (query) => {
-    if (query.length < 2) {
-        return { results: [] };
-    }
-
     try {
-        const response = await axios.get(`${BASE_URL}/games`, {
+        const response = await axios.get(`/api/games`, {
             params: {
-                key: API_KEY,
-                search: query,
-                page_size: 10,
+                query,
             },
         });
         return response.data;
@@ -41,11 +31,7 @@ export const searchGames = async (query) => {
 
 export const fetchGameDetails = async (gameSlug) => {
     try {
-        const response = await axios.get(`${BASE_URL}/games/${gameSlug}`, {
-            params: {
-                key: API_KEY,
-            },
-        });
+        const response = await axios.get(`/api/games/${gameSlug}`);
         return response.data;
     } catch (error) {
         console.error("Error fetching game details:", error);
@@ -55,11 +41,7 @@ export const fetchGameDetails = async (gameSlug) => {
 
 export const fetchGameScreenshots = async (gameSlug) => {
     try {
-        const response = await axios.get(`${BASE_URL}/games/${gameSlug}/screenshots`, {
-            params: {
-                key: API_KEY,
-            },
-        });
+        const response = await axios.get(`/api/games/${gameSlug}/screenshots`);
         return response.data;
     } catch (error) {
         console.error("Error fetching game screenshots:", error);
