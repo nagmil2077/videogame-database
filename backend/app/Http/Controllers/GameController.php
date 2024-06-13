6b028bea91b07e83d1af7923b6df5591663bb5fd
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-13 12:44:35 +0200

    Create fetchAllGames method in GameController

--- backend/app/Http/Controllers/GameController.php
+++ backend/app/Http/Controllers/GameController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class GameController extends Controller
 {
+    private $apiKey;
+    private $baseUrl;
 
+    public function __construct()
+    {
+        $this->apiKey = config('services.rawg.key');
+        $this->baseUrl = 'https://api.rawg.io/api';
+    }
+
+    public function fetchAllGames(Request $request)
+    {
+        $page = $request->query('page', 1);
+        $pageSize = $request->query('page_size', 10);
+
+        $response = Http::get("{$this->baseUrl}/games", [
+            'key' => $this->apiKey,
+            'page' => $page,
+            'page_size' => $pageSize,
+        ]);
+
+        return response()->json($response->json());
+    }
 }
