c6074d7616afd8f41a7af7e653fd0474eba39a71
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-15 12:13:16 +0200

    Write code in LoginPage.jsx

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import LoginForm from '../Components/LoginForm';
+import './RegisterPage.css';
+
+const loginUser = (formData) => {
+  return axios.post('http://127.0.0.1:8000/api/login', formData);
+};
 
 const LoginPage = () => {
+    const navigate = useNavigate();
 
-}
+    const handleSubmit = (formData) => {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    return (
+        <div className="register-page-container">
+            <h2 className="register-title">Login</h2>
+            <LoginForm
+                handleSubmit={handleSubmit}
+                onCancel={() => navigate('/')}
+            />
+        </div>
+    );
+};
 
 export default LoginPage;
