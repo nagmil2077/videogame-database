782a4a34d28194d210d8e1dcd96e45d5190d6bfc
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-17 12:14:47 +0200

    Update Login Page to use auth context

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import LoginForm from '../Components/LoginForm';
 import axios from 'axios';
+import { AuthContext } from '../contexts/AuthContext';
 import './FormPage.css';
 
 const loginUser = (formData) => {
@@ -10,12 +11,14 @@ const loginUser = (formData) => {
 
 const LoginPage = () => {
     const navigate = useNavigate();
+    const { login } = useContext(AuthContext);
 
     const handleSubmit = async (formData) => {
         try {
             const response = await loginUser(formData);
             const { token, user } = response.data;
             localStorage.setItem('auth_token', token);
+            login(user);
             alert('Login successful');
             navigate('/');
         } catch (error) {
