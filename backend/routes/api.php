cc7d7b700c88b43f4d2db6a84a68386f8e39eee6
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-06-17 14:31:12 +0200

    Define logout route in api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +18,7 @@ Route::get('/search', [GameController::class, 'searchGames']);
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [LogoutController::class, 'logout']);
+});
