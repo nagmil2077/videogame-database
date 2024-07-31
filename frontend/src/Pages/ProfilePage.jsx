defc694ec5d6f662a67427220258a08504e71948
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-07-31 10:49:28 +0200

    Fix authContext import and switch button order in ProfilePage

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import {useContext} from "react";
-import { AuthContext } from '../contexts/AuthContext';
+import { AuthContext } from '../Contexts/AuthContext';
 import {Button, Container} from "react-bootstrap";
 import './FormPage.css';
 
@@ -10,8 +10,8 @@ const ProfilePage = () => {
         <div className="form-page-container">
             <h2 className="form-title">Welcome, {user.name}!</h2>
             <Container className="d-flex flex-column align-items-center">
-                <Button variant="primary" className="my-2" size="lg">Update Profile</Button>
                 <Button variant="primary" className="my-2" size="lg">View Favorites</Button>
+                <Button variant="primary" className="my-2" size="lg">Update Profile</Button>
                 <Button variant="danger" className="my-2" size="lg">Delete Profile</Button>
             </Container>
         </div>
