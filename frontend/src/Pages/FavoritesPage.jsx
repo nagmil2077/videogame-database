dbed66c001cec9cf4f25633802765b77b587dc9a
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-08-02 12:37:29 +0200

    Add message to frontend in case of empty favorites list

--- frontend/src/Pages/FavoritesPage.jsx
+++ frontend/src/Pages/FavoritesPage.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {getFavorites} from "../Services/favoritesService";
-import { Button, Container, Image, ListGroup } from 'react-bootstrap';
+import {Button, Container, Image, ListGroup} from 'react-bootstrap';
 import './FavoritesPage.css';
 
 const fetchFavorites = async () => {
@@ -25,23 +25,33 @@ const FavoritesPage = () => {
         <div className="favorites-page">
             <Container>
                 <h2>Your Favorite Games</h2>
-                <ListGroup>
-                    {favorites.map(game => (
-                        <ListGroup.Item key={game.id} className="favorite-item">
-                            <Image src={game.background_image} alt={game.name} className="favorite-thumbnail"/>
-                            <div className="favorite-details">
+                {favorites.length > 0 ? (
+                    <ListGroup>
+                        {favorites.map(game => (
+                            <ListGroup.Item key={game.id} className="favorite-item">
+                                <Image src={game.background_image} alt={game.name} className="favorite-thumbnail"/>
+                                <div className="favorite-details">
                                 <span
                                     className="favorite-name"
-                                    onClick={() => navigate(`/games/${game.slug}`)}>{game.name}
+                                    onClick={() => navigate(`/games/${game.slug}`)}>
+                                    {game.name}
                                 </span>
-                                <span className="favorite-year">({new Date(game.released).getFullYear()})</span>
-                            </div>
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
+                                    <span className="favorite-year">({new Date(game.released).getFullYear()})</span>
+                                </div>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                ) : (
+                    <p className="no-favorites-message">You don't have any favorite games yet. Start by adding some!</p>
+                )}
                 <br/>
-                <Button variant="secondary" onClick={() => navigate("/profile")}>Back to Profile Page</Button>
-                <Button variant="secondary" onClick={() => navigate("/")}>Back to Main Page</Button>
+                <div>
+                    <Button variant="secondary" onClick={() => navigate("/profile")}>Back to Profile Page</Button>
+                </div>
+                <br/>
+                <div>
+                    <Button variant="secondary" onClick={() => navigate("/")}>Back to Main Page</Button>
+                </div>
             </Container>
         </div>
     );
