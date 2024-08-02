7a06aac871799f911480310ab20c3e3c6abf16a8
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-08-02 10:03:38 +0200

    Implement code for FavoritesPage.jsx

--- frontend/src/Pages/FavoritesPage.jsx
+++ frontend/src/Pages/FavoritesPage.jsx
@@ -1,17 +1,49 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import axios from 'axios';
+import {getFavorites} from "../Services/favoritesService";
+import { Button, Container, Image, ListGroup } from 'react-bootstrap';
 
 const fetchFavorites = async () => {
     try {
-
+        return await getFavorites();
     } catch (error) {
-
+        console.error("Error fetching favorite games:", error);
     }
-}
+};
 
 const FavoritesPage = () => {
+    const [favorites, setFavorites] = useState([]);
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        fetchFavorites()
+            .then(data => setFavorites(data.favorites));
+    }, []);
+
+    return (
+        <div className="favorites-page">
+            <Container>
+                <h2>Your Favorite Games</h2>
+                <ListGroup>
+                    {favorites.map(game => (
+                        <ListGroup.Item key={game.id} className="favorite-item">
+                            <Image src={game.background_image} alt={game.name} className="favorite-thumbnail"/>
+                            <div className="favorite-details">
+                                <span
+                                    className="favorite-name"
+                                    onClick={() => navigate(`/games/${game.slug}`)}>{game.name}
+                                </span>
+                                <span className="favorite-year">({new Date(game.released).getFullYear()})</span>
+                            </div>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+                <br/>
+                <Button variant="secondary" onClick={() => navigate("/profile")}>Back to Profile Page</Button>
+                <Button variant="secondary" onClick={() => navigate("/")}>Back to Main Page</Button>
+            </Container>
+        </div>
+    );
 }
 
 export default FavoritesPage;
