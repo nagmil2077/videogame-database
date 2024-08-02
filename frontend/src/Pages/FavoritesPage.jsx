b599604050770bb87232cc4f2073f67fbf6ab44c
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-08-02 12:59:22 +0200

    Add loading animation to favorite games list

--- frontend/src/Pages/FavoritesPage.jsx
+++ frontend/src/Pages/FavoritesPage.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {getFavorites} from "../Services/favoritesService";
 import {Button, Container, Image, ListGroup} from 'react-bootstrap';
+import ClipLoader from 'react-spinners/ClipLoader';
 import './FavoritesPage.css';
 
 const fetchFavorites = async () => {
@@ -14,41 +15,51 @@ const fetchFavorites = async () => {
 
 const FavoritesPage = () => {
     const [favorites, setFavorites] = useState([]);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
         fetchFavorites()
-            .then(data => setFavorites(data.favorites));
+            .then(data => setFavorites(data.favorites))
+            .then(() => setLoading(false));
     }, []);
 
     return (
         <div className="favorites-page">
             <Container>
                 <h2>Your Favorite Games</h2>
-                {favorites.length > 0 ? (
-                    <ListGroup>
-                        {favorites.map(game => (
-                            <ListGroup.Item key={game.id} className="favorite-item">
-                                <Image src={game.background_image} alt={game.name} className="favorite-thumbnail"/>
-                                <div className="favorite-details">
+                {loading ? (
+                    <div className="spinner-container">
+                        <ClipLoader color="#000" loading={loading} size={50} />
+                    </div>
+                ) : (
+                    <>
+                        {favorites.length > 0 ? (
+                            <ListGroup>
+                                {favorites.map(game => (
+                                    <ListGroup.Item key={game.id} className="favorite-item">
+                                        <Image src={game.background_image} alt={game.name} className="favorite-thumbnail"/>
+                                        <div className="favorite-details">
                                 <span
                                     className="favorite-name"
                                     onClick={() => navigate(`/games/${game.slug}`)}>
                                     {game.name}
                                 </span>
-                                    <span className="favorite-year">({new Date(game.released).getFullYear()})</span>
-                                </div>
-                            </ListGroup.Item>
-                        ))}
-                    </ListGroup>
-                ) : (
-                    <p className="no-favorites-message">You don't have any favorite games yet. Start by adding some!</p>
+                                            <span className="favorite-year">({new Date(game.released).getFullYear()})</span>
+                                        </div>
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        ) : (
+                            <p className="no-favorites-message">You don't have any favorite games yet. Start by adding some!</p>
+                        )}
+                    </>
                 )}
-                <br/>
+                <br />
                 <div>
                     <Button variant="secondary" onClick={() => navigate("/profile")}>Back to Profile Page</Button>
                 </div>
-                <br/>
+                <br />
                 <div>
                     <Button variant="secondary" onClick={() => navigate("/")}>Back to Main Page</Button>
                 </div>
