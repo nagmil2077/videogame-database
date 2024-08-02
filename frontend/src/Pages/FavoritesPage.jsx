aa76c2967ec5d9ed349bca38206653e1cd69a609
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-08-02 09:46:04 +0200

    Create FavoritesPage.jsx

---
+++ frontend/src/Pages/FavoritesPage.jsx
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from 'axios';
+
+const fetchFavorites = async () => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const FavoritesPage = () => {
+
+}
+
+export default FavoritesPage;
